9010df07a16bcc97a3c7d77b88c5bdf1cbefbf04
vladimirB <vladimirzq@gmail.com>
2021-03-08 22:14:30 +0600

    generate models plus product CRUD

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -281,7 +281,9 @@ AppAsset::register($this);
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= Yii::$app->user->identity->displayName ?></span>
+                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                                <?= Yii::$app->user->isGuest ? 'Guest' : Yii::$app->user->identity->displayName; ?>
+                            </span>
                             <img class="img-profile rounded-circle"
                                  src="img/undraw_profile.svg">
                         </a>
---
+++ common/models/CartItem.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%cart_item}}".
+ *
+ * @property int $id
+ * @property int|null $product_id
+ * @property int $quantity
+ * @property int|null $created_by
+ *
+ * @property User $createdBy
+ * @property Product $product
+ */
+class CartItem extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%cart_item}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['product_id', 'quantity', 'created_by'], 'integer'],
+            [['quantity'], 'required'],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+            [['product_id'], 'exist', 'skipOnError' => true, 'targetClass' => Product::className(), 'targetAttribute' => ['product_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'product_id' => 'Product ID',
+            'quantity' => 'Quantity',
+            'created_by' => 'Created By',
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[Product]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\ProductQuery
+     */
+    public function getProduct()
+    {
+        return $this->hasOne(Product::className(), ['id' => 'product_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\CartItemQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\CartItemQuery(get_called_class());
+    }
+}
