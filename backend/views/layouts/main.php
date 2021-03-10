1c9af752603b596494220cec408542c53a8ce4d4
vladimirB <vladimirzq@gmail.com>
2021-03-10 23:24:48 +0600

    remove categories from backend main layout, change bootstrap 3 on 4 widgets on frontend layout

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -5,6 +5,7 @@
 /* @var $content string */
 
 use backend\assets\AppAsset;
+use backend\controllers\ProductController;
 use yii\helpers\Html;
 
 AppAsset::register($this);
@@ -30,7 +31,7 @@ AppAsset::register($this);
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
         <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= Yii::$app->homeUrl ?>">
             <div class="sidebar-brand-text mx-3">Yii2 E-commerce</div>
         </a>
 
@@ -39,7 +40,7 @@ AppAsset::register($this);
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="#">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
                 <span>Dashboard</span></a>
         </li>
@@ -53,29 +54,30 @@ AppAsset::register($this);
         </div>
 
         <!-- Nav Item - Pages Collapse Menu -->
-        <li class="<?= get_class($this->context) === \backend\controllers\ProductController::class ? 'nav-item active' : 'nav-item' ?>">
-            <?= Html::a('<i class="fas fa-fw fa-cog"></i><span>Products</span>',
-                ['product/'],
-                ['class' => 'nav-link']) ?>
-        </li>
-        <!-- Nav Item - Utilities Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-               aria-expanded="true" aria-controls="collapseUtilities">
-                <i class="fas fa-fw fa-wrench"></i>
-                <span>Categories</span>
-            </a>
-            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                 data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Custom Utilities:</h6>
-                    <a class="collapse-item" href="utilities-color.html">Colors</a>
-                    <a class="collapse-item" href="utilities-border.html">Borders</a>
-                    <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                    <a class="collapse-item" href="utilities-other.html">Other</a>
-                </div>
-            </div>
-        </li>
+        <?= Html::tag('li',
+                Html::a(
+                    '<i class="fas fa-fw fa-cog"></i><span>Products</span>', ['product/'],
+                    ['class' => 'nav-link']
+                ),
+            ['class' => get_class($this->context) === ProductController::class ? 'nav-item active' : 'nav-item']) ?>
+        <!--        // Nav Item - Utilities Collapse Menu -->
+        <!--        <li class="nav-item">-->
+        <!--            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"-->
+        <!--               aria-expanded="true" aria-controls="collapseUtilities">-->
+        <!--                <i class="fas fa-fw fa-wrench"></i>-->
+        <!--                <span>Categories</span>-->
+        <!--            </a>-->
+        <!--            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"-->
+        <!--                 data-parent="#accordionSidebar">-->
+        <!--                <div class="bg-white py-2 collapse-inner rounded">-->
+        <!--                    <h6 class="collapse-header">Custom Utilities:</h6>-->
+        <!--                    <a class="collapse-item" href="utilities-color.html">Colors</a>-->
+        <!--                    <a class="collapse-item" href="utilities-border.html">Borders</a>-->
+        <!--                    <a class="collapse-item" href="utilities-animation.html">Animations</a>-->
+        <!--                    <a class="collapse-item" href="utilities-other.html">Other</a>-->
+        <!--                </div>-->
+        <!--            </div>-->
+        <!--        </li>-->
 
         <!-- Divider -->
         <hr class="sidebar-divider">
